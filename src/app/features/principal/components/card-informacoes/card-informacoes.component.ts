18e1b07af2a87e320e0c71a5f8bf9225d9fbc2be
Bruno Felipe de Souza Barbosa <c112954@corp.caixa.gov.br>
2024-03-22 02:39:09 -0300

    ajustando responsividade do componente albuns e card-informacoes

--- src/app/features/principal/components/card-informacoes/card-informacoes.component.ts
+++ src/app/features/principal/components/card-informacoes/card-informacoes.component.ts
@@ -7,8 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardInformacoesComponent implements OnInit {
 
+  public boasVindas: string = 'Bem Vinda(a)!';
+  public subtitulo: string = 'A sua galeria de fotos Online';
+  public descricao: string = 'Explore 5.000 fotografias e 100 álbuns. Navegue por uma interface intuitiva que te leva aos álbuns que você deseja visitar com facilidade.';
 
-  urlFoto: string = './assets/img/img.svg';
 
   constructor() { }
 
