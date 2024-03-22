cf7a75d72274d7199d314eb73ee1af8294eca83b
Bruno Felipe de Souza Barbosa <c112954@corp.caixa.gov.br>
2024-03-22 00:41:39 -0300

    feat: estilizado os card com os abuns

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,5 @@
 import { Routes } from '@angular/router';
 import { LayoutComponent } from './core/layout/layout.component';
-import { HomeComponent } from './features/home/home.component';
 
 export const routes: Routes = [
   {
